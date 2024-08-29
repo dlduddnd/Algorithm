bf7a7ee395e933e53535adc5024aa1676b4d573c
이영웅 <162437227+dlduddnd@users.noreply.github.com>
2024-08-29 15:35:53 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,46 @@
+from collections import deque
+import sys
+
+d = deque()
+a = int(sys.stdin.readline())
+
+for tmp in range(a):
+    b = sys.stdin.readline().rstrip()
+
+    if "push" in b:
+        tmp,value = b.split()
+
+        d.append(int(value))
+
+    if b == 'pop':
+        if len(d) == 0:
+            print(-1)
+        else:
+            print(d[0])
+            d.popleft()
+
+
+    if b == 'size':
+        print(len(d))
+
+    if b == 'empty':
+        if len(d) == 0:
+            print(1)
+        else:
+            print(0)
+
+
+    if b == 'front':
+        
+        if len(d) == 0:
+            print(-1)
+        else:
+            print(d[0])
+
+    if b == 'back':
+        
+        if len(d) == 0:
+            print(-1)
+        else:
+            print(d[-1])
+
