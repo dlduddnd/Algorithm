b731c2df86724f0c5a0e72a909d04aabed5d3787
이영웅 <162437227+dlduddnd@users.noreply.github.com>
2024-11-03 15:09:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.76 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,13 @@
+def solution(nums):
+    answer = 0
+    s = set(nums)
+    
+    if len(nums)/2 < len(s):
+        answer = len(nums)/2
+    else:
+        answer = len(s)
+        
+    
+    return answer
+
+## set사용
